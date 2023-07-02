2f10a0e141b168ff7e831575ee9dc63b5b63fbdc
enscma2 <karina.halevy@gmail.com>
2023-07-02 18:00:48 +0200

    less generation data

--- my_image/entrypoint.sh
+++ my_image/entrypoint.sh
@@ -4,5 +4,5 @@ ${CONDA} run -n myenv python3 -m experiments.evaluate \
     --alg_name=MEMIT \
     --model_name=EleutherAI/gpt-j-6B \
     --hparams_fname=EleutherAI_gpt-j-6B.json \
-    --num_edits=10000 \
+    --num_edits=1 \
     --use_cache
