4c9d5d0743b728471a9d9a62bb8b9778f6ff9f0f
enscma2 <karina.halevy@gmail.com>
2023-07-02 19:01:31 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,8 @@ ${CONDA} run -n myenv python3 -m experiments.evaluate \
     --hparams_fname=EleutherAI_gpt-j-6B.json \
     --num_edits=1 \
     --use_cache
+    --skip_generation
+
+git add -f results/MEMIT/*/*.json
+git commit -m "results"
+git push
