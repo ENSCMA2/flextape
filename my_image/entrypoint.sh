fe7970ea09a67a76134189474917322abccf2bd5
enscma2 <karina.halevy@gmail.com>
2023-07-03 00:06:20 +0200

    i was lied to

--- my_image/entrypoint.sh
+++ my_image/entrypoint.sh
@@ -5,8 +5,7 @@ ${CONDA} run -n myenv python3 -m experiments.evaluate \
     --model_name=gpt2-xl \
     --hparams_fname=gpt2-xl.json \
     --num_edits=1 \
-    --use_cache \
-    --results_dir=/mnt/nlpdata1/home/halevy/flextape/results/MEMIT
+    --use_cache
 
 git add -f results/MEMIT/*/*.json
 git commit -m "results"
