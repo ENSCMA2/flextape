737e4d52a90c6e5a94fd922283330cf86240660e
enscma2 <karina.halevy@gmail.com>
2023-07-02 22:56:33 +0200

    all changes in order

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ ${CONDA} run -n myenv python3 -m experiments.evaluate \
     --model_name=gpt2-xl \
     --hparams_fname=gpt2-xl.json \
     --num_edits=1 \
-    --use_cache
+    --use_cache \
+    --results_dir=/mnt/u14157_ic_nlp_001_files_nfs/nlpdata1/home/halevy/flextape/results/MEMIT
 
 git add -f results/MEMIT/*/*.json
 git commit -m "results"
