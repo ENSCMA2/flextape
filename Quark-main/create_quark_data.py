1bbbc96d9188f0b0818ba8b107c9186d43785dd4
Karina Halevy <karina.halevy@gmail.com>
2023-07-11 18:58:00 +0200

    tts

--- Quark-main/create_quark_data.py
+++ Quark-main/create_quark_data.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 import jsonlines
+import random
 
 def json_to_jsonl(og, new_name):
     with open(og) as o:
@@ -11,9 +12,15 @@ def json_to_jsonl(og, new_name):
         prompt = prompt_temp.replace("{}", stuff["requested_rewrite"]["subject"])
         response = stuff["requested_rewrite"]["target_new"]["str"]
         ultimate.append({"prompt": prompt, "response": response})
-    with open(new_name, "w") as o:
-        for stuff in ultimate:
+    train = random.sample([i for i in range(len(ultimate))], int(len(ultimate) * 0.75))
+    ultimate_train = [ultimate[i] for i in train]
+    ultimate_val = [ultimate[i] for i in range(len(ultimate)) if i not in train]
+    with open(f"{new_name}_train.jsonl", "w") as o:
+        for stuff in ultimate_train:
+            o.write(str(stuff) + "\n")
+    with open(f"{new_name}_val.jsonl", "w") as o:
+        for stuff in ultimate_val:
             o.write(str(stuff) + "\n")
 
-json_to_jsonl("../data/seesaw_cf_P101_False_100.json", "../data/quark_P101_100.jsonl")
-json_to_jsonl("../data/seesaw_cf_P103_False_100.json", "../data/quark_P103_100.jsonl")
+json_to_jsonl("../data/seesaw_cf_P101_False_100.json", "../data/quark_P101_100")
+json_to_jsonl("../data/seesaw_cf_P103_False_100.json", "../data/quark_P103_100")
