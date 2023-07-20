f15ba388d8999d45e7d9d5b272782017b60527b2
Karina Halevy <karina.halevy@gmail.com>
2023-07-20 20:43:44 +0000

    more conflicts

--- tpatcher/src/models/gpt_module.py
+++ tpatcher/src/models/gpt_module.py
@@ -38,7 +38,7 @@ class GPTJSeq2Seq(LightningModule):
         parser.add_argument("--num_beams", type=int, default=NUM_BEAMS)
         # parser.add_argument("--lr", type=float, default=3e-5)
         parser.add_argument("--max_length", type=int, default=32)
-        parser.add_argument("--weight_decay", type=int, default=0.01)
+        # parser.add_argument("--weight_decay", type=int, default=0.01)
         parser.add_argument("--total_num_updates", type=int, default=50000)
         parser.add_argument("--warmup_updates", type=int, default=500)
         parser.add_argument("--num_workers", type=int, default=0)
