24e563e4f5b3a28ba409c95bf8dc34666ca5440e
enscma2 <karina.halevy@gmail.com>
2023-07-21 02:47:26 +0200

    submitting

--- tpatcher/src/models/gpt_module.py
+++ tpatcher/src/models/gpt_module.py
@@ -275,7 +275,7 @@ class GPTJEditor(nn.Module):
             }
         else:
             raise ValueError('Device is not enough!')
-        self.model.model.parallelize(device_map={0: [_ for _ in range(28)]})
+        self.model.model.parallelize(device_map=device_map)
         self.original_model = copy.deepcopy(model)
         self.hidden_size = hidden_size
         self.device = device
@@ -422,8 +422,8 @@ class GPTJEditor(nn.Module):
         self.detectors = []
         self.model.eval()
         self.model.model.parallelize(device_map = {
-                0: [_ for _ in range(0, 28)],
-                # 1: [_ for _ in range(14, 28)],
+                0: [_ for _ in range(0, 14)],
+                1: [_ for _ in range(14, 28)],
             })
         self.get_detectors(
             detected_modules=self.detected_modules,
@@ -585,7 +585,7 @@ class GPTJEditor(nn.Module):
             }
         else:
             raise ValueError('Device is not enough!')
-        self.model.model.parallelize(device_map={0: [_ for _ in range(0, 28)]})
+        self.model.model.parallelize(device_map=device_map)
         # model_named_modules = {x[0]:x[1] for x in self.model.model.transformer.named_modules()}
         # for key, value in self.device_distribution.items():
         #
