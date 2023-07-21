b8c1a030201d779f8fb6051d354e8140040c83f6
enscma2 <karina.halevy@gmail.com>
2023-07-21 18:31:47 +0200

    seriously

--- my_image/entrypoint.sh
+++ my_image/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git pull
 
-cd Quark-main
-${CONDA} run -n myenv python main.py
+cd tpatcher
+${CONDA} run -n myenv python src/dataset/zsre_dataloader.py
+${CONDA} run -n myenv python scripts/main.py
 
 git add -A
-git add -f outputs/*
 git commit -m "results"
 git push
