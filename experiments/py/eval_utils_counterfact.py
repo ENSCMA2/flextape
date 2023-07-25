2e86f2c8ff6eef545ebf0d5f87cc5c1fc41989ad
Karina Halevy <karina.halevy@gmail.com>
2023-07-25 19:48:09 +0000

    preedit

--- experiments/py/eval_utils_counterfact.py
+++ experiments/py/eval_utils_counterfact.py
@@ -47,7 +47,7 @@ def compute_rewrite_quality_counterfact(
     rewrite_prompts = [record["requested_rewrite"]["prompt"].format(subject)]
     # neighborhood_prompts = record["neighborhood_prompts"]
     generation_prompts = record["generation_prompts"]
-    attribute_prompts = [record["attribute_prompts"][-1]]
+    attribute_prompts = record["attribute_prompts"]
     # Form a list of lists of prefixes to test.
     prob_prompts = [
         # rewrite_prompts,
