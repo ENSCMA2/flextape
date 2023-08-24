897a241d771ba22dfc1a2d1a048252f4c7e9a25a
Karina Halevy <karina.halevy@gmail.com>
2023-08-24 16:23:01 +0000

    cuh

--- dsets/counterfact.py
+++ dsets/counterfact.py
@@ -22,7 +22,7 @@ class CounterFactDataset(Dataset):
         self.data = []
         data_dir = Path(data_dir)
         cf_loc = data_dir / (
-            f"seesaw_cf_P103_False_100.json"
+            f"seesaw_cf_P101_False_100.json"
         )
         if not cf_loc.exists():
             remote_url = f"{REMOTE_ROOT}/{'multi_' if multi else ''}counterfact.json"
--- experiments/preedit.py
+++ experiments/preedit.py
@@ -142,7 +142,6 @@ def main(
                 "grouped_case_ids": case_ids,
                 "num_edits": num_edits,
                 "requested_rewrite": record["requested_rewrite"],
-                # "time": exec_time,
                 "pre": ds_eval_method(
                     model,
                     tok,
@@ -153,6 +152,7 @@ def main(
                         else [None, None]
                     ),
                 ),
+                "time": time() - start
             }
 
             # Dump metrics in .json
