2b0922b2abd0c544dfef3d27ebe0126408942d44
Karina Helen Halevy <khalevy@babel-login-2.lti.cs.cmu.edu>
2024-03-27 18:32:32 -0400

    fix sh

--- eval_llama.sh
+++ eval_llama.sh
@@ -1,2 +1,2 @@
-${CONDA} run -n myenv python -m experiments.preedit --ds_name=$1 --alg_name=NONE --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=EleutherAI_gpt-j-6B.json --num_edits=900 --use_cache;
-${CONDA} run -n myenv python -m experiments.evaluate --ds_name=$1 --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=EleutherAI_gpt-j-6B.json --num_edits=900 --use_cache;
+python -m experiments.preedit_llama --ds_name=$1 --alg_name=NONE --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache;
+python -m experiments.evaluate_llama --ds_name=$1 --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache;
