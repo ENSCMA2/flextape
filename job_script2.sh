088a1b6fc66b7daab27557b525d63e97f975c9b8
Karina Helen Halevy <khalevy@babel-login-3.lti.cs.cmu.edu>
2024-05-27 15:00:39 -0400

    wait

--- job_script2.sh
+++ job_script2.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=flextapebig.err
 #SBATCH --partition=long
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:9
+#SBATCH --gres=gpu:10
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=80GB
 #SBATCH --cpus-per-task=16
@@ -12,6 +12,6 @@
 
 for DS in "P101" "P103";
 do
-python -m experiments.preedit_mistral --ds_name=$DS --alg_name=NONE --model_name=mistralai/Mistral-7B-v0.1 --hparams_fname=mistral-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
+# python -m experiments.preedit_mistral --ds_name=$DS --alg_name=NONE --model_name=mistralai/Mistral-7B-v0.1 --hparams_fname=mistral-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
 python -m experiments.evaluate_mistral --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=mistralai/Mistral-7B-v0.1 --hparams_fname=mistral-7b.json --num_edits=900 --use_cache --skip_generation;
 done
