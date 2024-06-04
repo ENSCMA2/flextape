c3328e3419540eaf17ad425609337a5d40705a1b
Karina Helen Halevy <khalevy@babel-login-1.lti.cs.cmu.edu>
2024-06-04 12:08:10 -0400

    stashing

--- job_script.sh
+++ job_script.sh
@@ -10,9 +10,10 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=100:00:00
 
-bash eval_pair_llama.sh meta-llama/Llama-2-7b-chat-hf;
+# bash eval_pair_llama.sh meta-llama/Llama-2-7b-chat-hf;
 
-for DS in "P21_P101" "P27_P101" "P19_P101" "P101" "P103";
+# for DS in "P21_P101" "P27_P101" "P19_P101" "P101" "P103";
+for DS in "P103";
 do
 python -m experiments.preedit_llama --ds_name=$DS --alg_name=NONE --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
 python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation;
