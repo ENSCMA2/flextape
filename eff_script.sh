50146025d3c4b5c31c4145ebe069ee9b12d5428f
Karina Helen Halevy <khalevy@babel-login-4.lti.cs.cmu.edu>
2024-06-05 00:19:12 -0400

    girl

--- eff_script.sh
+++ eff_script.sh
@@ -4,14 +4,14 @@
 #SBATCH --error=flextape.err
 #SBATCH --partition=long
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=80GB
 #SBATCH --cpus-per-task=16
 #SBATCH --time=100:00:00
 
-for DS in "P103";
-# "P21_P101" "P27_P101" "P19_P101" "P101" "P103";
+for DS in "P103" ;
+# "P21_P101" "P27_P101" "P19_P101" "P101";
 do
 python -m experiments.preedit_llama --ds_name=$DS --alg_name=NONE --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001 --eff
 python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --eff;
