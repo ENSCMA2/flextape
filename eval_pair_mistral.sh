b2d4b2928ec99f942cf1d23df937c9e2acda3d96
Karina Helen Halevy <khalevy@babel-login-2.lti.cs.cmu.edu>
2024-06-11 14:52:12 -0400

    commit

--- eval_pair_mistral.sh
+++ eval_pair_mistral.sh
@@ -1,7 +1,7 @@
 for DS in P101_P21 P27_P19 P21_P101 P27_P101 P27_P21 P27_P19 P101_P27 P19_P21 P19_P101
 do 
 	echo $DS;
-	python -m experiments.preedit_pair_mistral --ds_name=$DS --alg_name=NONE --model_name=$1 --hparams_fname=mistral-7b.json --num_edits=901 --use_cache --skip_generation_tests --continue_from_run=run_000;
+	# python -m experiments.preedit_pair_mistral --ds_name=$DS --alg_name=NONE --model_name=$1 --hparams_fname=mistral-7b.json --num_edits=901 --use_cache --skip_generation_tests --continue_from_run=run_000;
 done
 
 for DS in P101_P21 P27_P19 P21_P101 P27_P101 P27_P21 P27_P19 P101_P27 P19_P21 P19_P101
--- job_script.sh
+++ job_script.sh
@@ -2,18 +2,19 @@
 #SBATCH --job-name=flextape
 #SBATCH --output=flextape.out
 #SBATCH --error=flextape.err
-#SBATCH --partition=long
+#SBATCH --partition=long,general,r3lit
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=80GB
 #SBATCH --cpus-per-task=16
-#SBATCH --time=100:00:00
+#SBATCH --time=2-00:00:00
 
-bash eval_pair_llama.sh meta-llama/Llama-2-7b-chat-hf;
 
-for DS in "P21_P101" "P27_P101" "P19_P101" "P101" "P103";
+for DS in "P27_P101" "P21_P101" "P19_P101" "P103" "P101";
 do
-python -m experiments.preedit_llama --ds_name=$DS --alg_name=NONE --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
-python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation;
+# python -m experiments.preedit_llama --ds_name=$DS --alg_name=NONE --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
+python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation;
 done
+
+bash eval_pair_llama.sh meta-llama/Llama-2-7b-hf
