b2c5f187877bd971613ba805799e3117d9ea696c
Karina Helen Halevy <khalevy@babel-login-3.lti.cs.cmu.edu>
2024-06-15 14:32:46 -0400

    final push

--- job_script.sh
+++ job_script.sh
@@ -11,10 +11,10 @@
 #SBATCH --time=2-00:00:00
 
 
-for DS in "P27_P101" "P21_P101" "P19_P101" "P103" "P101";
-do
+# for DS in "P27_P101" "P21_P101" "P19_P101" "P103" "P101";
+# do
 # python -m experiments.preedit_llama --ds_name=$DS --alg_name=NONE --model_name=meta-llama/Llama-2-7b-chat-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation --continue_from_run=run_001
-python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation;
-done
+# python -m experiments.evaluate_llama --continue_from_run=run_001 --ds_name=$DS --alg_name=MEMIT --model_name=meta-llama/Llama-2-7b-hf --hparams_fname=llama-7b.json --num_edits=900 --use_cache --skip_generation;
+# done
 
 bash eval_pair_llama.sh meta-llama/Llama-2-7b-hf
